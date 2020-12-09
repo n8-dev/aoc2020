71d475dd0e75d0756fd70b501febc6ebb52369c6
Nate Devereux <nate.devereux@blackpepper.co.nz>
2020-12-10 11:40:48 +1300

    Restarted day4.
    Got a good looking array now in the data.

--- day4/part1.php
+++ day4/part1.php
@@ -1,11 +1,24 @@
 <?
 
-require_once('data.php');
 
 
-echo count($passports) . '
-';
+$fileContent = file(__DIR__ . "/data.txt");
 
+$array = [];
+$string = '';
+foreach ($fileContent as $line) {
+	if (strpos($line, ':') > 0) {
+		$string .= $line;
+	}
+	else {
+		// clean up spacing
+		$string = preg_replace('/\R/', ' ', $string);
+		$array[] = $string;
+		$string = '';
+	}
+}
+
+print_r($array);
 
 
 $requiredFields = [
@@ -16,30 +29,7 @@ $requiredFields = [
 	'hcl', // (Hair Color)
 	'ecl', // (Eye Color)
 	'pid', // (Passport ID)
-	//'cid', // (Country ID)
+	//'cid', // (Country ID) // optional
 ];
 $count = 0;
 $valid = 0;
-foreach ($passports as $passport){
-	echo $passport .'
-';
-	$currentValid = 0;
-
-	foreach ($requiredFields as $key) {	
-		if (strpos($passport , $key) != FALSE) {
-			$currentValid ++;
-		}
-	}
-echo 'valid count: '.  $currentValid . '
-
-';
-	if ($currentValid == 7) {
-		$valid ++;
-	}
-	$count++;
-
-	if ($count == 10) {
-		break;
-	}
-}
-echo $valid;
